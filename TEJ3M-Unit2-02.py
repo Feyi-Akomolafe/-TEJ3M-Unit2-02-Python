4331f1ec1c64a387261b32287bac62fc67249c42
Feyi Akomolafe <113041424+Feyi-Akomolafe@users.noreply.github.com>
2023-04-04 20:17:39 -0400

    Create TEJ3M-Unit2-02.py

---
+++ TEJ3M-Unit2-02.py
@@ -0,0 +1,20 @@
+# Copyright(c) 2023 Feyi Akomolafe All rights reserved.
+# Created by : Feyi Akomolafe
+# Created on : Fabruary 2023
+# TEJ3M-Unit2-02.py File.
+
+import time
+import board
+import digitalio
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+blinkTime = 1
+
+while True:
+    led.value = True
+    time.sleep(blinkTime)
+    led.value = False
+    time.sleep(1)
+    blinkTime += 1
